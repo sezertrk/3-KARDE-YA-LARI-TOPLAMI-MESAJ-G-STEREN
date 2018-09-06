dc560aaeadb67f26fc412473289a1a1ac75ba7d2
sezertrk <43032011+sezertrk@users.noreply.github.com>
2018-09-06 15:06:32 +0300

    Add files via upload

---
+++ 3 KARDEŞ YAŞLARI TOPLAMI MESAJ GÖSTEREN/3 KARDEŞ YAŞLARI TOPLAMI MESAJ GÖSTEREN/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _3_KARDEŞ_YAŞLARI_TOPLAMI_MESAJ_GÖSTEREN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int k1 = int.Parse (textBox1.Text);
+            int k2 = int .Parse (textBox2.Text);
+            int k3 = int .Parse (textBox3.Text);
+            int son;
+            son = k1 + k2 + k3;
+            MessageBox.Show  (son.ToString());
+                
+        }
+    }
+}
---
+++ 3 KARDEŞ YAŞLARI TOPLAMI MESAJ GÖSTEREN/3 KARDEŞ YAŞLARI TOPLAMI MESAJ GÖSTEREN/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace _3_KARDEŞ_YAŞLARI_TOPLAMI_MESAJ_GÖSTEREN
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
